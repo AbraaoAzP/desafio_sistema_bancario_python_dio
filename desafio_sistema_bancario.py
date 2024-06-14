835e23d8e9afe538ccc39b1bd72319d208b36b61
AbraaoAzP <abraaoazp@gmail.com>
2024-06-14 20:40:25 -0300

    Projeto Finalizado

--- desafio_sistema_bancario.py
+++ desafio_sistema_bancario.py
@@ -1,5 +1,7 @@
 menu = """
 
+Bem-vindo ao DioBank, por favor selecione uma das operações abaixo:
+
 [1] Depositar
 [2] Sacar
 [3] Extrato
@@ -18,38 +20,42 @@ while True:
     opcao = input(menu)
 
     if opcao == "1":
-        
-        print("Informe o valor a ser depositado: ")
+        deposito = float(input("Informe o valor do depósito: "))
 
-        deposito = int(input())
-        saldo += deposito
-        extrato = deposito
+        if deposito > 0:
+            saldo += deposito
+            extrato += f"Depósito: R${deposito:.2f}\n"
+            print("Depósito realizado com sucesso!")
 
-        print("Depósito realizado com sucesso!")
+        else:
+            print("Operação não realizada! O valor do depósito é inválido.")
 
     elif opcao == "2":
-
-        print("Informe o valor a ser sacado: ")
-        saque = int(input())
+        saque = float(input("Informe o valor a ser sacado: "))
 
         if saldo == 0 or saque > saldo:
-            print("Não é possível realizar o saque! Saldo insuficiente.")
+            print("Operação não realizada! Saldo em conta insuficiente.")
+
+        elif saque > limite:
+            print("Operação não realizada! O valor do saque excede o limite permitido.")
 
         elif numero_saques >= LIMITE_SAQUES:
-            print("Não é possível realizar o saque! O limite diário permitido foi atingido!")
+            print("Operação não realizada! Limite de saques diários excedido.")
         
-        elif saque <= limite:
+        elif saque > 0:
             saldo -= saque
-            extrato = saque
+            extrato += f"Saque: R${saque:.2f}\n"
             numero_saques += 1
             print("Saque realizado com sucesso!")
 
         else:
-            print("Não é possível realizar o saque! O valor excede o limite permitido!")
+            print("Operação não realizada! O valor informado é inválido.")
 
     elif opcao == "3":
-        print("Extrato")
-        print(f"R${extrato:.2f}")
+        print("\n====== Dio Bank - Extrato ======")
+        print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R${saldo:.2f}")
+        print("\n================================")
 
     elif opcao == "0":
         print("Obrigado por usar nosso sistema.")
